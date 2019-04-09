e6cbd9482128f82087dbe80d8ab325240797725f
tdincer1 <talhadincer96@hotmail.com>
2019-04-09 21:02:40 +0300

    Son hali

---
+++ app/src/main/java/com/squad/testdeneme/Tercih_Robotu/TercihDb.java
@@ -0,0 +1,44 @@
+package com.squad.testdeneme.Tercih_Robotu;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.squad.testdeneme.MySQLiteHelper;
+import com.squad.testdeneme.QuizDbHelper;
+
+public class TercihDb {
+    private static TercihDb INSTANCE;
+    private static Context context;
+
+    private static SQLiteDatabase database;
+    private static MySQLiteHelper myhelper; //Veritabanina erisim icin MySQLiteHelper objesi olusturduk
+
+    public TercihDb() {
+
+        myhelper = new MySQLiteHelper(context);
+    }
+
+    public static TercihDb getInstance(Context context) {
+
+        TercihDb.context = context;
+
+        if(INSTANCE == null)
+        {
+            INSTANCE = new TercihDb();
+        }
+        return INSTANCE;
+    }
+
+    private void openDB() throws SQLException {
+
+        database = myhelper.getWritableDatabase();
+    }
+
+    private void closeDB() {
+
+        database.close();
+    }
+
+    
+}

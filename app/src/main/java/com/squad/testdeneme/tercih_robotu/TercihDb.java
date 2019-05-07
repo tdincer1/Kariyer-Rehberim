d4b71efc32d71c5ba3f94cbb5ac1469d741599ba
tdincer1 <talhadincer96@hotmail.com>
2019-05-07 21:58:31 +0300

    KtAnaSayfa ve MtAnaSayfa sayfaları eklendi..

--- app/src/main/java/com/squad/testdeneme/tercih_robotu/TercihDb.java
+++ app/src/main/java/com/squad/testdeneme/tercih_robotu/TercihDb.java
@@ -69,7 +69,7 @@ public class TercihDb {
                 bilgi.setPr_kodu(cTr.getInt(cTr.getColumnIndex("bolum_id")));
                 bilgi.setBolum(cTr.getString(cTr.getColumnIndex("bolum_adi")));
                 bilgi.setDil(cTr.getString(cTr.getColumnIndex("dil")));
-                bilgi.setBolum_turu(cTr.getString(cTr.getColumnIndex("burs")));
+                bilgi.setBolum_turu(cTr.getString(cTr.getColumnIndex("bolum_turu")));
                 bilgi.setTaban_puani(cTr.getString(cTr.getColumnIndex("taban_puani")));
                 bilgi.setSiralama(cTr.getInt(cTr.getColumnIndex("basari_sirasi")));
                 bilgi.setKontenjan(cTr.getInt(cTr.getColumnIndex("kontenjan")));
@@ -116,10 +116,10 @@ public class TercihDb {
         if(sehir.length()!=0) {
             queryy += " AND il_adi = '" + sehir + "'";
         }
-        if(bolumTuru.length()!=0 && bolumTuru.length()!=5) {    //length=5 cunku spinnerdan hepsi 5 uzunlugunda geliyo
-            queryy += " AND burs = '" + bolumTuru + "'";
+        if(bolumTuru.length()!=0 && bolumTuru.length()!=4) {    //length=5 cunku spinnerdan hepsi 5 uzunlugunda geliyo
+            queryy += " AND bolum_turu = '" + bolumTuru + "'";
         }
-        if(puanTuru.length()!=0 && puanTuru.length()!=5) {
+        if(puanTuru.length()!=0 && puanTuru.length()!=4) {
             queryy += " AND puan_turu = '" + puanTuru + "'";
         }
         if ((siralamaMax.length()!=0) && (siralamaMin.length()!=0) ) queryy += " AND basari_sirasi BETWEEN " + siralamaMin + " AND " + siralamaMax;
@@ -148,7 +148,7 @@ public class TercihDb {
                 bilgi.setPr_kodu(crs.getInt(crs.getColumnIndex("bolum_id")));
                 bilgi.setBolum(crs.getString(crs.getColumnIndex("bolum_adi")));
                 bilgi.setDil(crs.getString(crs.getColumnIndex("dil")));
-                bilgi.setBolum_turu(crs.getString(crs.getColumnIndex("burs")));
+                bilgi.setBolum_turu(crs.getString(crs.getColumnIndex("bolum_turu")));
                 bilgi.setTaban_puani(crs.getString(crs.getColumnIndex("taban_puani")));
                 bilgi.setSiralama(crs.getInt(crs.getColumnIndex("basari_sirasi")));
                 bilgi.setKontenjan(crs.getInt(crs.getColumnIndex("kontenjan")));
@@ -184,7 +184,7 @@ public class TercihDb {
             meslek = " LIKE '%Öğretmenliği%'";
             meslekSorgu = sorguSade + meslek;
         }else if ("Temel Bilimler".equalsIgnoreCase(meslek)){
-            meslek = " IN ('Fizik','Kimya','Matematik')";
+            meslek = " IN ('Fizik','Kimya','Matematik','Biyoloji')";
             meslekSorgu = sorguSade + meslek;
         }else if ("Dil ve Edebiyat Bölümleri".equalsIgnoreCase(meslek)){
             meslek = " LIKE '%Dili ve Edebiyatı%'";
@@ -205,7 +205,7 @@ public class TercihDb {
                 bilgi.setPr_kodu(ccc.getInt(ccc.getColumnIndex("bolum_id")));
                 bilgi.setBolum(ccc.getString(ccc.getColumnIndex("bolum_adi")));
                 bilgi.setDil(ccc.getString(ccc.getColumnIndex("dil")));
-                bilgi.setBolum_turu(ccc.getString(ccc.getColumnIndex("burs")));
+                bilgi.setBolum_turu(ccc.getString(ccc.getColumnIndex("bolum_turu")));
                 bilgi.setTaban_puani(ccc.getString(ccc.getColumnIndex("taban_puani")));
                 bilgi.setSiralama(ccc.getInt(ccc.getColumnIndex("basari_sirasi")));
                 bilgi.setKontenjan(ccc.getInt(ccc.getColumnIndex("kontenjan")));

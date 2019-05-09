bcdefc152028c8b1a7d71acf22939daae2e7e8f0
alkimercan <alkimercan_119@hotmail.com>
2019-05-09 12:32:43 +0300

    alkımdan yengeye çeyrek altın

--- app/src/main/java/com/squad/testdeneme/AnaEkran.java
+++ app/src/main/java/com/squad/testdeneme/AnaEkran.java
@@ -1,16 +1,21 @@
 package com.squad.testdeneme;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.squad.testdeneme.kisilik_testi.KtAnaSayfa;
 import com.squad.testdeneme.meslek_testi.KonservatuarEkran;
 import com.squad.testdeneme.meslek_testi.MtAnaSayfa;
 import com.squad.testdeneme.tercih_robotu.TercihFiltre;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class AnaEkran extends AppCompatActivity {
@@ -19,6 +24,10 @@ public class AnaEkran extends AppCompatActivity {
     Button buttonTercihRobotu;
     Button buttonKisilikTesti;
     Button konser;
+    private TextView txtTimerDay, txtTimerHour, txtTimerMinute, txtTimerSecond;
+    private TextView tvEvent;
+    private Handler handler;
+    private Runnable runnable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +39,13 @@ public class AnaEkran extends AppCompatActivity {
         buttonKisilikTesti = findViewById(R.id.button_kisilik_start);
         konser = findViewById(R.id.btn_konser);
 
+        txtTimerDay = (TextView) findViewById(R.id.txtTimerDay);
+        txtTimerHour = (TextView) findViewById(R.id.txtTimerHour);
+        txtTimerMinute = (TextView) findViewById(R.id.txtTimerMinute);
+        txtTimerSecond = (TextView) findViewById(R.id.txtTimerSecond);
+        tvEvent = (TextView) findViewById(R.id.tvhappyevent);
 
+        countDownStart();
 
         konser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,4 +85,54 @@ public class AnaEkran extends AppCompatActivity {
         startActivity(intent);
         //startActivityForResult(intent, REQUEST_CODE_QUIZ);
     }
+    public void countDownStart() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(
+                            "yyyy-MM-dd");
+                    // Please here set your event date//YYYY-MM-DD
+                    Date futureDate = dateFormat.parse("2019-06-15");
+                    Date currentDate = new Date();
+                    if (!currentDate.after(futureDate)) {
+                        long diff = futureDate.getTime()
+                                - currentDate.getTime();
+                        long days = diff / (24 * 60 * 60 * 1000);
+                        diff -= days * (24 * 60 * 60 * 1000);
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+                        txtTimerDay.setText("" + String.format("%02d", days));
+                        txtTimerHour.setText("" + String.format("%02d", hours));
+                        txtTimerMinute.setText(""
+                                + String.format("%02d", minutes));
+                        txtTimerSecond.setText(""
+                                + String.format("%02d", seconds));
+                    } else {
+                        tvEvent.setVisibility(View.VISIBLE);
+                        tvEvent.setText("Sınav Yapılmıştır !");
+                        textViewGone();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1 * 1000);
+    }
+
+    public void textViewGone() {
+        findViewById(R.id.LinearLayout10).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout11).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout12).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout13).setVisibility(View.GONE);
+        findViewById(R.id.textView1).setVisibility(View.GONE);
+        findViewById(R.id.textView2).setVisibility(View.GONE);
+    }
 }
+
--- app/src/main/java/com/squad/testdeneme/meslek_testi/KonservatuarEkran.java
+++ app/src/main/java/com/squad/testdeneme/meslek_testi/KonservatuarEkran.java
@@ -2,6 +2,7 @@ package com.squad.testdeneme.meslek_testi;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import com.squad.testdeneme.R;
@@ -11,5 +12,7 @@ public class KonservatuarEkran extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_konservatuar_ekran);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.hide();
     }
 }
